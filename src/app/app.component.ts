9ba50987ed66c5d137b859a41c9fb7ddd2fdfeb3
prkpwm <prkpwm.dev@gmail.com>
2023-06-15 11:45:40 +0700

    recheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,36 +13,60 @@ export class AppComponent {
   answer = '';
   currentSearch = 1;
   waiting = false;
-  @ViewChild('searchInput') searchInput:any;
-  constructor(private http: HttpClient) { }
+
+
+
+  @ViewChild('searchInput') searchInput: any;
+  constructor(private http: HttpClient) {
+    const path = window.location.pathname;
+    if (path.length > 1) {
+      this.waiting = true;
+      this.http
+        .post<any>('https://prkpwm-url-shortener.onrender.com/api/originalUrl', { shortUrl: path.replace('/', '') })
+        .subscribe((response) => {
+          if (response.originalUrl && /Not Found/.test(response.originalUrl) === false) {
+            if (response.originalUrl.indexOf('http') === -1) {
+              response.originalUrl = 'http://' + response.originalUrl;
+            }
+            this.waiting = false;
+            window.open(response.originalUrl, '_self');
+
+          }
+        }
+        );
+    }
+  }
+
 
   shortenUrl(): void {
+    if (!this.originalUrl) return;
     this.waiting = true;
-    if(!this.originalUrl) return;
     this.http
-      .post<any>('http://localhost:3000/api/shorten', { originalUrl: this.originalUrl })
+      .post<any>('https://prkpwm-url-shortener.onrender.com/api/shorten', { originalUrl: this.originalUrl })
       .subscribe((response) => {
         this.shortUrl = response.shortUrl;
         this.waiting = false;
+        this.answer = window.location.href + response.shortUrl;
 
       });
   }
 
   originalyUrl() {
+    if (!this.shortUrl) return;
     this.waiting = true;
-    if(!this.shortUrl) return;
     this.http
-      .post<any>('http://localhost:3000/api/originalUrl', { shortUrl: this.shortUrl })
+      .post<any>('https://prkpwm-url-shortener.onrender.com/api/originalUrl', { shortUrl: this.shortUrl })
       .subscribe((response) => {
         this.originalUrl = response.originalUrl;
         this.waiting = false;
+        this.answer = window.location.href + response.shortUrl;
       });
   }
 
   search() {
-    if(this.originalUrl) {
+    if (this.originalUrl) {
       this.shortenUrl();
-    }else{
+    } else {
       this.originalyUrl();
     }
 
@@ -57,4 +81,5 @@ export class AppComponent {
   onKeydown() {
     if (this.searchInput.nativeElement.value.length < 3) return;
   }
+
 }
